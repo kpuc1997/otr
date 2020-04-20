bcc992e7a2852e30b0d0cf8dc5211cd5d4b34cd2
Kyle <me@kyleclapper.com>
2020-04-19 23:32:41 -0400

    Updates

---
+++ precache-manifest.c28ab2ae02a3bf0a91ba05048ee91f25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ab63bd4d59329adc940d9e1206bc48",
+    "url": "/otr/index.html"
+  },
+  {
+    "revision": "99bbd81ae4f9f513c91b",
+    "url": "/otr/static/css/main.c9eaa74b.chunk.css"
+  },
+  {
+    "revision": "8f18a712849fd8b8e24b",
+    "url": "/otr/static/js/2.3932df48.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/otr/static/js/2.3932df48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99bbd81ae4f9f513c91b",
+    "url": "/otr/static/js/main.bd2328b4.chunk.js"
+  },
+  {
+    "revision": "f05e74b77c71b1c6d407",
+    "url": "/otr/static/js/runtime-main.30e6d316.js"
+  },
+  {
+    "revision": "09241dd8f2342902ba9cbbeead19a429",
+    "url": "/otr/static/media/WUML 2.09241dd8.png"
+  },
+  {
+    "revision": "d3f0a6fb884d48ec85a4985f2ea4ff5d",
+    "url": "/otr/static/media/bee.d3f0a6fb.png"
+  }
+]);
