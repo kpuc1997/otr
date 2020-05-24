2365a685ff52403125758b0e9d841e4e47829ef6
Kyle <me@kyleclapper.com>
2020-05-24 15:32:15 -0400

    Fixed the radio player embed in the navbar

--- src/components/Misc/wumlplayer.js
+++ src/components/Misc/wumlplayer.js
@@ -4,6 +4,6 @@ import React from 'react';
 // May look weird at widths below 400px.
 // Uses default export.
 
-const player = <iframe title="stream" src="https://embed.radio.co/player/fa3c718.html" width="100%" allow="autoplay" scrolling="no" style={{border: 'none', overflow: 'hidden', maxWidth: '400px', margin: '0px auto', height: '100px',}}></iframe>;
+const player = <iframe title="stream" src="https://embed.radio.co/player/fa3c718.html?popout" width="100%" allow="autoplay" scrolling="no" style={{border: 'none', overflow: 'hidden', maxWidth: '400px', margin: '0px auto', height: '100px',}}></iframe>;
 
 export default player;
