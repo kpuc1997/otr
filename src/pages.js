b9c2253e690ce2408ce866195b272d4417f13971
Kyle <me@kyleclapper.com>
2020-08-05 19:52:20 -0400

    Added terms of service and enabled service worker for PWA functionality

--- src/pages.js
+++ src/pages.js
@@ -4,6 +4,7 @@ import Recordings from './recordings';
 import Skating from './matadorskating';
 import Poll from './mmmPoll';
 import Playlists from './playlists';
+import TOS from './termsofservice';
 
 const pages = [
     {name:"Staff", url:"/Staff", component: Staff},
@@ -12,6 +13,7 @@ const pages = [
     {name: "Munch, Marry, Murder", url:'/mmm', component: Poll},
     {name:"Brain Powder", url:'/BrainPowder', component: BrainPowder},
     {name:"Matador Skating", url:"/Skating", component: Skating},
+    {name:'Terms of Service', url:'/TermsofService', component: TOS}
     
 ];
 
